4b9bc4a1b2907bd7adc3b20ff40f5dd933510ed7
OlleDelborg <ollend2@gmail.com>
2022-05-21 11:35:00 +0000

    ansible

--- start-instances.py
+++ start-instances.py
@@ -1,40 +1,38 @@
 """
 Source:
-	Create new volume - YES
-	Delete volume on Instance Delete - YES
-	Image: Ubuntu 20.04 - 2021.03.23
+        Create new volume - YES
+        Delete volume on Instance Delete - YES
+        Image: Ubuntu 20.04 - 2021.03.23
 Flavor:
-	ssc.medium
+        ssc.medium
 Network:
-	UPPMAX 2022/1-1 Internal IPv4 Network
+        UPPMAX 2022/1-1 Internal IPv4 Network
 Security Groups:
-	- default: UPPMAX 2022/1-1 default security group
+        - default: UPPMAX 2022/1-1 default security group
 Key Pair
-	- olzmanskeyz
+        - olzmanskeyz, update this
 Configuration:
-	- cloud-config.txt
-
-Make sure that the following is installed (Ubuntu 20.04):
-	- sudo add-apt-repository cloud-archive:wallaby
-    - sudo apt install nova-compute
-    - sudo apt install python3-openstackclient
+        -
 """
 
-import time, os, sys, random
+#http://docs.openstack.org/developer/python-novaclient/ref/v2/servers.html
+import time, os, sys, random, re
 import inspect
 from os import environ as env
-
-from novaclient import client
+from  novaclient import client
 import keystoneclient.v3.client as ksclient
 from keystoneauth1 import loading
 from keystoneauth1 import session
 
-server_name = "group_12_server_"+str(random.randint(1000,9999))
+
 flavor = "ssc.medium" 
 private_net = "UPPMAX 2022/1-1 Internal IPv4 Network"
 floating_ip_pool_name = None
 floating_ip = None
 image_name = "Ubuntu 20.04 - 2021.03.23"
+
+identifier = random.randint(1000,9999)
+
 loader = loading.get_plugin_loader('password')
 
 auth = loader.load_from_options(auth_url=env['OS_AUTH_URL'],
@@ -42,6 +40,7 @@ auth = loader.load_from_options(auth_url=env['OS_AUTH_URL'],
                                 password=env['OS_PASSWORD'],
                                 project_name=env['OS_PROJECT_NAME'],
                                 project_domain_id=env['OS_PROJECT_DOMAIN_ID'],
+                                #project_id=env['OS_PROJECT_ID'],
                                 user_domain_name=env['OS_USER_DOMAIN_NAME'])
 
 sess = session.Session(auth=auth)
@@ -49,9 +48,8 @@ nova = client.Client('2.1', session=sess)
 print ("user authorization completed.")
 
 image = nova.glance.find_image(image_name)
-print("NO IMAGE ERROR")
+
 flavor = nova.flavors.find(name=flavor)
-print("NO FLAVOR ERROR")
 
 if private_net != None:
     net = nova.neutron.find_network(private_net)
@@ -61,26 +59,53 @@ else:
 
 #print("Path at terminal when executing this file")
 #print(os.getcwd() + "\n")
-cfg_file_path =  os.getcwd()+'/cloud-config.txt'
+cfg_file_path =  os.getcwd()+'/producer-cfg.txt'
 if os.path.isfile(cfg_file_path):
-    userdata = open(cfg_file_path)
+    userdata_prod = open(cfg_file_path)
 else:
-    sys.exit("cloud-config.txt is not in current working directory")
+    sys.exit("producer-cfg.txt is not in current working directory")
+
+cfg_file_path =  os.getcwd()+'/consumer-cfg.txt'
+if os.path.isfile(cfg_file_path):
+    userdata_dev = open(cfg_file_path)
+else:
+    sys.exit("consumer-cfg.txt is not in current working directory")    
 
 secgroups = ['default']
 
-print ("Creating instance ... ")
-
-instance = nova.servers.create(name=server_name, image=image, flavor=flavor, key_name='olzmanskeyz',userdata=userdata, nics=nics, security_groups=secgroups)
-inst_status = instance.status
+print ("Creating instances ... ")
+instance_prod = nova.servers.create(name="group_12_producer_"+str(identifier), image=image, flavor=flavor, key_name='olzmanskeyz',userdata=userdata_prod, nics=nics,security_groups=secgroups)
+instance_dev = nova.servers.create(name="group_12_consumer_"+str(identifier), image=image, flavor=flavor, key_name='olzmanskeyz',userdata=userdata_dev, nics=nics,security_groups=secgroups)
+inst_status_prod = instance_prod.status
+inst_status_dev = instance_dev.status
 
 print ("waiting for 10 seconds.. ")
 time.sleep(10)
 
-while inst_status == 'BUILD':
-    print ("Instance: "+instance.name+" is in "+inst_status+" state, sleeping for 5 seconds more...")
+while inst_status_prod == 'BUILD' or inst_status_dev == 'BUILD':
+    print ("Instance: "+instance_prod.name+" is in "+inst_status_prod+" state, sleeping for 5 seconds more...")
+    print ("Instance: "+instance_dev.name+" is in "+inst_status_dev+" state, sleeping for 5 seconds more...")
     time.sleep(5)
-    instance = nova.servers.get(instance.id)
-    inst_status = instance.status
+    instance_prod = nova.servers.get(instance_prod.id)
+    inst_status_prod = instance_prod.status
+    instance_dev = nova.servers.get(instance_dev.id)
+    inst_status_dev = instance_dev.status
 
-print ("Instance: "+ instance.name +" is in " + inst_status + " state")
+ip_address_prod = None
+for network in instance_prod.networks[private_net]:
+    if re.match('\d+\.\d+\.\d+\.\d+', network):
+        ip_address_prod = network
+        break
+if ip_address_prod is None:
+    raise RuntimeError('No IP address assigned!')
+
+ip_address_dev = None
+for network in instance_dev.networks[private_net]:
+    if re.match('\d+\.\d+\.\d+\.\d+', network):
+        ip_address_dev = network
+        break
+if ip_address_dev is None:
+    raise RuntimeError('No IP address assigned!')
+
+print ("Instance: "+ instance_prod.name +" is in " + inst_status_prod + " state" + " ip address: "+ ip_address_prod)
+print ("Instance: "+ instance_dev.name +" is in " + inst_status_dev + " state" + " ip address: "+ ip_address_dev)
